61c74d2fd6a5377f9a025048122bffc7d1365614
Nilesh kumar <162776615+NileshKumar2404@users.noreply.github.com>
2026-09-06 19:01:31 +0530

    feat: add notification routes

---
+++ src/routes/notification.routes.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import {
+    getNotifications,
+    markNotificationRead,
+    markAllNotificationsRead
+} from '../controllers/notification.controller.js'
+
+const router = express.Router()
+
+router.get('/', getNotifications)
+router.patch('/:id/read', markNotificationRead)
+router.patch('/read-all', markAllNotificationsRead)
+
+export default router
